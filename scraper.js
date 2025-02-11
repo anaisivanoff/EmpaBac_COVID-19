4b680f56e4fc8d77900cfc6d0e95e2f12f4d0a2c
ivanov <anais.ivanov@ynov.com>
2025-02-11 12:48:19 +0100

    session 2020

---
+++ scraper.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  // 1. Lancer le navigateur
+  const browser = await puppeteer.launch({ headless: true }); // "false" pour voir le navigateur s'ouvrir
+  const page = await browser.newPage();
+
+  // 2. Aller sur le site cible
+  await page.goto('https://www.studyrama.com/revision-examen/bac/actualite/bac-2020-les-chiffres-definitifs-108549');
+
+  // 3. Attendre que la page soit chargée complètement
+  await page.waitForSelector('div'); // Attendre l'élément principal
+
+  // 4. Extraire du texte de la page
+  const title = await page.evaluate(() => {
+    return document.querySelector('body').innerText;
+  });
+
+  console.log("Bac 2020 : les chiffres définitifs :", title);
+
+  // 5. Fermer le navigateur
+  +
+  await browser.close();
+})();
