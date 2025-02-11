777fe7030f3036fce19f9efe71fdd09d3e8299e0
ivanov <anais.ivanov@ynov.com>
2025-02-11 14:47:46 +0100

    session 2019

--- scraper2.js
+++ scraper2.js
@@ -4,14 +4,14 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({ headless: true }); // "false" pour voir le navigateur s'ouvrir
   const page = await browser.newPage();
   // 2. Aller sur le site cible
-  await page.goto('https://www.studyrama.com/revision-examen/bac/actualite/bac-2020-les-chiffres-definitifs-108549');
+  await page.goto('https://groupe-reussite.fr/ressources/blog/evolution-taux-reussite-bac/');
   // 3. Attendre que la page soit chargée complètement
-  await page.waitForSelector('div'); // Attendre l'élément principal
+  await page.waitForSelector('html'); // Attendre l'élément principal
   // 4. Extraire du texte de la page
   const title = await page.evaluate(() => {
     return document.querySelector('body').innerText;
   });
-  console.log("Bac 2020 : les chiffres définitifs :", title);
+  console.log("Taux de réussite au bac :évolution, résultats, mentions :", title);
   // 5. Fermer le navigateur
   +
   await browser.close();
