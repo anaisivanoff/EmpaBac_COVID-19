2fdd324ae15a926578eb6d196f7e4af645d1ff00
lenzo <enzo.ledda@ynov.com>
2025-02-14 09:44:43 +0100

    nettoyage_donnees.js (PAS FONCTIONNEL)

--- nettoyage_donnees.js
+++ nettoyage_donnees.js
@@ -18,61 +18,51 @@ const scrapedData = {
     "2024": JSON.parse(rawData2024),
 };
 
-
 function extraireTauxGeneral(texte) {
     const mots = texte.split(/\s+/);
     let resultats = [];
-
     for (let i = 0; i < mots.length; i++) {
         if (mots[i].match(/^\d+,\d+%?$/)) {
             let nombre = mots[i].replace('%', '');
             let contexte = mots.slice(Math.max(0, i - 5), i + 6);
-
             let cleanedContexte = contexte.join(' ').replace(/[\.,;:()]/g, '');
             if (cleanedContexte.toLowerCase().includes("général")) {
                 resultats.push(parseFloat(nombre.replace(',', '.')));
             }
         }
     }
-
     return resultats.length > 0 ? resultats[0] : null;
 }
 
 function extraireTauxTechnologique(texte) {
   const mots = texte.split(/\s+/);
   let resultats = [];
-
   for (let i = 0; i < mots.length; i++) {
       if (mots[i].match(/^\d+,\d+%?$/)) {
           let nombre = mots[i].replace('%', '');
           let contexte = mots.slice(Math.max(0, i - 5), i + 6);
-
           let cleanedContexte = contexte.join(' ').replace(/[\.,;:()]/g, '');
           if (cleanedContexte.toLowerCase().includes("technologique")) {
               resultats.push(parseFloat(nombre.replace(',', '.')));
           }
       }
   }
-
   return resultats.length > 0 ? resultats[0] : null;
 }
 
 function extraireTauxProfessionnel(texte) {
   const mots = texte.split(/\s+/);
   let resultats = [];
-
   for (let i = 0; i < mots.length; i++) {
       if (mots[i].match(/^\d+,\d+%?$/)) {
           let nombre = mots[i].replace('%', '');
           let contexte = mots.slice(Math.max(0, i - 5), i + 6);
-
           let cleanedContexte = contexte.join(' ').replace(/[\.,;:()]/g, '');
           if (cleanedContexte.toLowerCase().includes("professionnel")) {
               resultats.push(parseFloat(nombre.replace(',', '.')));
           }
       }
   }
-
   return resultats.length > 0 ? resultats[0] : null;
 }
 
@@ -97,7 +87,6 @@ function nettoyerDonnees(extractedText, annee) {
         }
     };
 
-
     if (annee !== "2019") {
         cleanedData.tauxReussite.general = tauxGeneral;
         cleanedData.tauxReussite.technologique = tauxTechnologique;
@@ -107,7 +96,6 @@ function nettoyerDonnees(extractedText, annee) {
     return cleanedData;
 }
 
-
 function processData() {
     let donneesFinales = {};
 
@@ -115,7 +103,6 @@ function processData() {
         donneesFinales[annee] = nettoyerDonnees(scrapedData[annee].extractedText, annee);
     }
 
-    
     fs.writeFileSync('donnees.json', JSON.stringify(donneesFinales, null, 2), 'utf8');
     console.log("Les données nettoyées ont été sauvegardées dans 'donnees.json'");
 }
