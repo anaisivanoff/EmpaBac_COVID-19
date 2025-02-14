a386809bb4686d53ff98cc51a480e64fe6f6f9a5
ivanov <anais.ivanov@ynov.com>
2025-02-14 11:31:52 +0100

    session 2021

--- scraper.js/scraper4.js
+++ scraper.js/scraper4.js
@@ -1,23 +1,17 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs'); // Ajoute le module fs pour écrire dans un fichier
+const fs = require('fs'); 
 
 (async () => {
-  // 1. Lancer le navigateur
-  const browser = await puppeteer.launch({ headless: true }); // "false" pour voir le navigateur s'ouvrir
+  const browser = await puppeteer.launch({ headless: true }); 
   const page = await browser.newPage();
 
-  // 2. Aller sur le site cible
   await page.goto('https://www.studyrama.com/revision-examen/bac/resultats-du-bac/bac-2021-les-resultats-provisoires-108636#:~:text=R%C3%A9sultats%20Bac%202021%20%3A%2093.8%25%20de%20r%C3%A9ussite%20avant%20la%20session%20de%20rattrapage', { waitUntil: 'domcontentloaded' });
+  await page.waitForSelector('html'); 
 
-  // 3. Attendre que la page soit chargée complètement
-  await page.waitForSelector('html'); // Attendre l'élément principal
-
-  // 4. Extraire du texte de la page
   const title = await page.evaluate(() => {
     return document.querySelector('body').innerText;
   });
   
-  // 5. Vérifier si le texte a été extrait et créer l'objet
   if (title) {
     const data = {
       extractedText: title,
@@ -25,13 +19,11 @@ const fs = require('fs'); // Ajoute le module fs pour écrire dans un fichier
       extractedAt: new Date().toISOString()
     };
 
-    // 6. Sauvegarder les données dans un fichier JSON
     fs.writeFileSync('data4.json', JSON.stringify(data, null, 2), 'utf-8');
     console.log("Les données ont été sauvegardées dans 'data4.json'");
   } else {
     console.log("Aucune donnée extraite.");
   }
 
-  // 7. Fermer le navigateur
   await browser.close();
 })();
